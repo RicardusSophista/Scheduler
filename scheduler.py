c7b79f49d24a49844340d1a1954b0fce793e1464
Richard Brown <richard.brown@cantab.net>
2020-12-23 21:22:28 +0000

    feat: Initial commit

---
+++ scheduler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 18:08:02 2020
+
+@author: richard
+
+TODO
+monthly, weekly, annual payment schedules
+FIRST = short month > first day of next month
+LAST = short month > last day of month
+
+YES = day1 is last day of month
+NO = day1 is regular day of month
+
+WEEKENDS = if NO, do not use weekends
+HOLIDAY = if populated, use list of holidays
+"""
+
