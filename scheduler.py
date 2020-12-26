09f7f8674c73145d2e0871bd1ba6c05004188e83
Richard Brown <richard.brown@cantab.net>
2020-12-26 22:11:21 +0000

    feat: Added basic ability to add week/month/year, no short-month treatment

--- scheduler.py
+++ scheduler.py
@@ -23,9 +23,30 @@ import dateutil as du
 class Schedule:
     def __init__(self, interval, increment, shortmonth="last", monthend="No",weekends="Keep",holidays=None):
         self.interval = interval
-        self.imcrement = increment
+        self.increment = increment
         self.shortmonth = shortmonth
         self.monthend = monthend
         self.weekends = weekends
         self.holidays = holidays
+
+    def nth_term(self, start, count, incr_1=None, underlying=None):
+        delta = count * self.increment
+        if self.interval == "week":
+            raw = start + du.relatvedelta.relativedelta(weeks =+ delta)
+        elif self.interval == "month":
+            raw = start + du.relativedelta.relativedelta(months =+ delta)
+        else:
+            raw = start + du.relativedelta.relativedelta(years =+ delta)
         
+        return raw
+        
+
+    def recite(start, stop=None, term=None, month_1=None):
+        sched = []
+
+sched = Schedule("month",1)
+start_input = input('Input start date >>> ')
+day, month, year = start_input.split('/')
+start = dt.datetime(int(year), int(month), int(day))
+
+print(sched.nth_term(start,1))
